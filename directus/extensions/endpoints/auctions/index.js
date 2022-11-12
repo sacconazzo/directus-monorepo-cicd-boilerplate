f7a52b2f8a5d9d9241516cba5b02875937283b2e
Giona Righini <sapbasis@alteanet.it>
2022-11-12 17:16:21 +0100

    oasconfig

--- directus/extensions/endpoints/auctions/index.js
+++ directus/extensions/endpoints/auctions/index.js
@@ -1,31 +1,27 @@
+const { validate } = require('directus-extension-api-docs');
+
 module.exports = {
     id: 'auctions',
     handler: function registerEndpoint(router, { services, exceptions, logger }) {
         const { ItemsService } = services;
         const { ServiceUnavailableException } = exceptions;
 
-        router.post('/register', async (req, res, next) => {
+        validate(router, ['/auctions/test']);
+
+        router.post('/test', async (req, res, next) => {
             try {
                 const item = new ItemsService('directus_users', {
                     schema: req.schema,
                     accountability: { admin: true },
                 });
 
-                const user = await item.readByQuery({
+                const [user] = await item.readByQuery({
                     filter: {
                         email: req.body.email,
-                        status: 'draft',
                     },
                 });
 
-                res.json({ user });
-
-                try {
-                    const awsService = new AWSService();
-                    await awsService.sendSMS('OTP: ' + req.body.otp, req.body.telephone, 'DeAngelis');
-                } catch (e) {
-                    logger.error(e);
-                }
+                res.json({ id: user?.id });
             } catch (error) {
                 return next(new ServiceUnavailableException(error.message || error[0].message));
             }
