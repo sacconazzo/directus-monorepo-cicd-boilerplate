d3ea7edc0b88de75713316bd6005bdd070eea338
Giona Righini <grighini@alteanet.it>
2024-06-24 11:52:26 +0200

    compat. fixes

--- directus/extensions/migrations/20220907B-settings-update.js
+++ directus/extensions/migrations/20220907B-settings-update.js
@@ -1,24 +1,10 @@
 const settings = {
     project_name: 'SCN API',
-    project_url: null,
-    project_color: null,
     project_logo: 'EBB42C0E-B56B-4D70-A32F-A31583E29A55',
-    public_foreground: null,
-    public_background: null,
-    public_note: null,
     auth_login_attempts: 25,
-    auth_password_policy: null,
     storage_asset_transform: 'all',
-    storage_asset_presets: null,
-    custom_css: null,
-    storage_default_folder: null,
-    basemaps: null,
-    mapbox_key: null,
-    module_bar: null,
     project_descriptor: 'backoffice',
-    translation_strings: null,
     default_language: 'it-IT',
-    custom_aspect_ratios: null,
 };
 
 module.exports = {
